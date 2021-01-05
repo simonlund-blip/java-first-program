2994e8abbf774ffb04599d1c83c8b0d7290fdd35
simonlund-blip <76995268+simonlund-blip@users.noreply.github.com>
2021-01-05 13:44:20 +0100

    1,2,3

---
+++ src/main/java/com/h2/SavingsCalculator.java
@@ -0,0 +1,10 @@
+package com.h2;
+
+public class SavingsCalculator
+{
+    private float[] credits;
+    private float[] debits;
+
+    public void Constructor(float[] credits, float[] debits) {
+    }
+}
